61b0135c2d27d3d80eb37720105a0662c411daad
prasadbobby <knvdurgaprasad610@gmail.com>
2025-10-03 00:15:12 +0530

    changed middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,10 @@ export default async function RootLayout({
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      signInUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL}
+      signUpUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL}
+      afterSignInUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL}
+      afterSignUpUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL}
     >
       <html
         lang="en"
--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,6 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/sign-in", "/sign-up"],
-
 });
 
 export const config = {
