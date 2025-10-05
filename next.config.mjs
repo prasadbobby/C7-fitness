a69d5ab4051ee20c297cfa05ea661275790c19cd
prasadbobby <knvdurgaprasad610@gmail.com>
2025-10-05 17:04:23 +0530

    feat: Add remote image patterns to next.config for improved image handling

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,16 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'shu7sxuwbwejohey.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
